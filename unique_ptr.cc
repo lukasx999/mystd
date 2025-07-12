e3fd7b09939af4948971e9c653ee9806882bb795
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-07-12 16:57:28 +0200

    added unique_ptr

---
+++ unique_ptr.cc
@@ -0,0 +1,77 @@
+#include <cassert>
+#include <print>
+#include <utility>
+
+template <typename T>
+class UniquePtr {
+    T* m_ptr = nullptr;
+
+public:
+    UniquePtr() = default;
+
+    explicit UniquePtr(T* ptr) : m_ptr(ptr) { }
+
+    ~UniquePtr() {
+        delete m_ptr;
+    }
+
+    UniquePtr(UniquePtr const&) = delete;
+    UniquePtr& operator=(UniquePtr const&) = delete;
+
+    UniquePtr(UniquePtr&& other)
+    : m_ptr(std::exchange(other.m_ptr, nullptr))
+    { }
+
+    UniquePtr& operator=(UniquePtr&& other) {
+        if (&other != this) {
+            reset(other.release());
+        }
+        return *this;
+    }
+
+    void swap(UniquePtr& other) {
+        std::swap(other.m_ptr, m_ptr);
+    }
+
+    T* release() {
+        return std::exchange(m_ptr, nullptr);
+    }
+
+    void reset(T* ptr) {
+        delete m_ptr;
+        m_ptr = ptr;
+    }
+
+    [[nodiscard]] T* get() {
+        return m_ptr;
+    }
+
+    T operator*() {
+        return *get();
+    }
+
+};
+
+template <typename T, typename... Args>
+[[nodiscard]] static constexpr inline
+UniquePtr<T> my_make_unique(Args&&... args) {
+    return UniquePtr<T>(new T(std::forward<Args>(args)...));
+}
+
+
+
+int main() {
+
+    UniquePtr foo = my_make_unique<int>(1);
+    assert(*foo == 1);
+
+    UniquePtr bar(std::move(foo));
+    assert(foo.get() == nullptr);
+    assert(*bar == 1);
+
+    UniquePtr baz = my_make_unique<int>(2);
+    baz = std::move(bar);
+    assert(*baz == 1);
+    assert(bar.get() == nullptr);
+
+}
